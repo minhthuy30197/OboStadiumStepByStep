2f80e1ff6e4fca7b69800a587375880b77cdd278
minhthuy30197 <minhthuy30197@gmail.com>
2021-02-21 21:02:26 +0700

    get post detail

--- 04-obo-stadium-gender/src/main/java/com/example/demo/controller/anonymous/BlogController.java
+++ 04-obo-stadium-gender/src/main/java/com/example/demo/controller/anonymous/BlogController.java
@@ -1,13 +1,17 @@
 package com.example.demo.controller.anonymous;
 
 import com.example.demo.entity.Post;
+import com.example.demo.exception.NotFoundException;
 import com.example.demo.service.BlogService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @Controller
 public class BlogController {
@@ -23,5 +27,20 @@ public class BlogController {
         model.addAttribute("listPost", posts.getContent());
 
         return "blog/blog";
+    }
+
+    @GetMapping("/tin-tuc/{slug}/{id}")
+    public String getPostDetailPage(Model model, @PathVariable long id) {
+        try {
+            Post post = blogService.getPostById(id);
+            model.addAttribute("post", post);
+        } catch (NotFoundException ex) {
+            return "error/404";
+        }
+
+        List<Post> latestPosts = blogService.getLatestPostsNotId(id);
+        model.addAttribute("latestPosts", latestPosts);
+
+        return "blog/detail";
     }
 }
--- 04-obo-stadium-gender/src/main/java/com/example/demo/service/BlogService.java
+++ 04-obo-stadium-gender/src/main/java/com/example/demo/service/BlogService.java
@@ -4,7 +4,13 @@ import com.example.demo.entity.Post;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public interface BlogService {
     Page<Post> getListPost(int page);
+
+    Post getPostById(long id);
+
+    List<Post> getLatestPostsNotId(long id);
 }
